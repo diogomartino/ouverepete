06e93de4e0fc57cec722c16c7e0c00e41b93a652
bruxo00 <diogo.martino10@gmail.com>
2022-03-11 21:40:00 +0000

    Added search

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import getCidadeFMData from '../../crawlers/cidade';
 import SongElement from '../song';
 import styles from './index.module.css';
@@ -13,7 +13,8 @@ const Home = () => {
     const [stats, setStats] = useState({
         repeatedSongs: 0,
         repeatedTimes: 0,
-    })
+    });
+    const [search, setSearch] = useState('');
 
     const fetchDataHandler = async () => {
         window.umami?.('Fetched data');
@@ -51,6 +52,22 @@ const Home = () => {
         setConfigs(newConfigs);
     }
 
+    const searchHandler = (event) => {
+        const text = event.target.value;
+        setSearch(text);
+    }
+
+    const filterSongs = (song) => {
+        const cleanSearch = search.trim().toLocaleLowerCase();
+        const cleanName = song.name.trim().toLocaleLowerCase();
+
+        if (!cleanSearch) {
+            return true;
+        }
+
+        return !!cleanName.includes(cleanSearch);
+    }
+
     return (
         <div className="container">
             <div>
@@ -82,13 +99,18 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="d-flex flex-column justify-content-center mt-2">
-                    <a className={styles.github} href="https://github.com/bruxo00/ouverepete" target="_blank">Github</a>
-                    <a className={styles.github} href="https://www.youtube.com/watch?v=4o84CvRJ9uw" target="_blank">Pela Música</a>
+                    <span className="text-center"><a className={styles.github} href="https://github.com/bruxo00/ouverepete" target="_blank">Github</a></span>
+                    <span className="text-center"><a className={styles.github} href="https://www.youtube.com/watch?v=4o84CvRJ9uw" target="_blank">Pela Música</a></span>
                 </div>
                 {!loading && cidadeData.length > 0 && (
                     <>
                         <div className="card mt-3">
                             <div className="card-body">
+                                <div>
+                                    <div class="input-group mb-3">
+                                        <input value={search} onChange={searchHandler} type="text" class="form-control" placeholder="Procura por uma música aqui..." aria-label="Nome" />
+                                    </div>
+                                </div>
                                 <div className={styles.items}>
                                     De <span className="fw-bold">{cidadeData.length}</span> músicas, <span className="fw-bold">{stats.repeatedSongs}</span> foram repetidas <span className="fw-bold">{stats.repeatedTimes}</span> vezes.
                                 </div>
@@ -97,7 +119,7 @@ const Home = () => {
                         <div className="card mt-3 mb-3">
                             <div className="card-body">
                                 <div className={styles.items}>
-                                    {cidadeData.map(element => {
+                                    {cidadeData.filter(filterSongs).map(element => {
                                         return <SongElement key={element.name} {...element} />;
                                     })}
                                 </div>
